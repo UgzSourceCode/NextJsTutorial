0e1ab254070569cd3ce55ea6b6d0a995932aa5db
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2021-01-06 23:33:17 +0100

    read and show data from md file on index page

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import * as matter from 'gray-matter';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <Layout home>
       <Head>
@@ -14,7 +15,37 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        {props.collect.map((item) => (
+          <div>
+            <p>{item.id} - {item.date} - {item.date}</p>
+            <p>{item.content}</p>
+          </div>
+        ))}
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const pre = matter.read("./posts/pre-rendering.md");
+  const ssd = matter.read("./posts/ssg-ssr.md");
+  
+  return {
+    props: {
+      collect: [
+        {
+          id: 1,
+          title: pre.data.title,
+          date: pre.data.date,
+          content: pre.content,
+        },
+        {
+          id: 2,
+          title: ssd.data.title,
+          date: ssd.data.date,
+          content: ssd.content,
+        },
+      ]
+    }
+  }
 }
